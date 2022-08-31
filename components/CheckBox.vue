22450253378aca9e6f14de025f3298034744ac10
Cameron Zollinger <cameron@zolly.dev>
2022-08-31 11:48:54 -0600

    add checkbox and toggle function

---
+++ components/CheckBox.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="checkbox-container" @click="toggle">
+    <input class="checkbox-input" type="checkbox" :isChecked="isChecked" />
+    <span
+      :id="id"
+      role="checkbox"
+      class="checkbox-checkbox"
+      :class="{ checked: isChecked }"
+    >
+      <ph-check v-if="isChecked" class="icon" />
+    </span>
+    <label :for="id">{{ label }}</label>
+  </div>
+</template>
+
+<script>
+import { PhCheck } from 'phosphor-vue'
+
+export default {
+  name: 'check-box',
+  components: {
+    PhCheck,
+  },
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+    label: {
+      type: String,
+      required: false,
+      default: null,
+    },
+    value: {
+      required: false,
+    },
+    checked: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      isChecked: this.checked,
+    }
+  },
+  methods: {
+    toggle(event) {
+      event.preventDefault()
+      console.log(this.value)
+      this.isChecked = !this.isChecked
+    },
+  },
+}
+</script>
+
+<style scoped>
+.checkbox-container {
+  display: flex;
+  gap: 8px;
+}
+.checkbox-input {
+  position: absolute;
+  width: 0;
+  height: 0;
+  opacity: 0;
+  pointer-events: none;
+}
+.checkbox-checkbox {
+  border: 1px solid #a9b6c7;
+  background-color: white;
+  border-radius: 4px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: all 0.2s;
+  width: 20px;
+  height: 20px;
+}
+.checked {
+  background-color: #20c4f4;
+  border-color: #20c4f4;
+}
+.icon {
+  transform: scale(1.25);
+  color: white;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,15 @@
   <div class="container">
     <h1>All Time</h1>
     <line-chart :dataPoints="chartData" />
+    <check-box
+      v-for="group in lineGroups"
+      :key="group"
+      :id="group"
+      checked="false"
+      :label="group"
+      style="margin-bottom: 4px"
+      :value="group"
+    />
     <h1>Results</h1>
     <table>
       <tr>
@@ -20,97 +29,125 @@
 
 <script>
 import LineChart from '../components/LineChart'
+import CheckBox from '../components/CheckBox'
 
 export default {
   name: 'IndexPage',
   components: {
     LineChart,
+    CheckBox,
   },
   data() {
     return {
       chartData: [
         {
+          id: 1,
           test: 'Recognition Details',
           date: new Date('2022-01-01'),
           time: 3000,
         },
         {
+          id: 2,
           test: 'Recognition Details',
           date: new Date('2022-03-03'),
           time: 1000,
         },
         {
+          id: 3,
           test: 'Recognition Details',
           date: new Date('2022-04-09'),
           time: 2367,
         },
         {
+          id: 4,
           test: 'Recognition Details',
           date: new Date('2022-06-12'),
           time: 4098,
         },
         {
+          id: 5,
           test: 'Recognition Details',
           date: new Date('2022-07-16'),
           time: 2098,
         },
         {
+          id: 6,
           test: 'Recognition Details',
           date: new Date('2022-11-20'),
           time: 3002,
         },
         {
+          id: 7,
           test: 'Recognition Details',
           date: new Date('2022-12-01'),
           time: 6000,
         },
         {
+          id: 8,
           test: 'Recognition Details',
           date: new Date('2022-12-20'),
           time: 8000,
         },
         {
+          id: 9,
           test: 'Points Redeemed',
           date: new Date('2022-01-01'),
           time: 1000,
         },
         {
+          id: 10,
           test: 'Points Redeemed',
           date: new Date('2022-03-21'),
           time: 800,
         },
         {
+          id: 11,
           test: 'Points Redeemed',
           date: new Date('2022-04-01'),
           time: 2000,
         },
         {
+          id: 12,
           test: 'Points Redeemed',
           date: new Date('2022-07-12'),
           time: 3209,
         },
         {
+          id: 13,
           test: 'Points Redeemed',
           date: new Date('2022-07-30'),
           time: 1999,
         },
         {
+          id: 14,
           test: 'Points Redeemed',
           date: new Date('2022-11-11'),
           time: 2831,
         },
         {
+          id: 15,
           test: 'Points Redeemed',
           date: new Date('2022-12-11'),
           time: 4000,
         },
         {
+          id: 16,
           test: 'Points Redeemed',
           date: new Date('2022-12-30'),
           time: 2000,
         },
       ],
     }
+  },
+  computed: {
+    lineGroups() {
+      let groups = new Set()
+      this.chartData.forEach((dataPoint) => {
+        groups.add(dataPoint.test)
+      })
+
+      return groups
+    },
   },
 }
 </script>
